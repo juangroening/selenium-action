58269dd4e1ddc7dad285f571d3b3a16efd55f004
juangroening <juan.groening@islandpitch.com>
2025-04-02 13:36:24 -0700

    ok

--- Selenium-Template.py
+++ Selenium-Template.py
@@ -3,38 +3,29 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 import chromedriver_autoinstaller
 from pyvirtualdisplay import Display
-display = Display(visible=0, size=(800, 800))  
+
+# Start virtual display
+display = Display(visible=0, size=(1200, 1200))  
 display.start()
 
-chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
-                                      # and if it doesn't exist, download it automatically,
-                                      # then add chromedriver to path
+# Install and setup ChromeDriver
+chromedriver_autoinstaller.install()
 
-chrome_options = webdriver.ChromeOptions()    
-# Add your options as needed    
-options = [
-  # Define window size here
-   "--window-size=1200,1200",
-    "--ignore-certificate-errors"
- 
-    #"--headless",
-    #"--disable-gpu",
-    #"--window-size=1920,1200",
-    #"--ignore-certificate-errors",
-    #"--disable-extensions",
-    #"--no-sandbox",
-    #"--disable-dev-shm-usage",
-    #'--remote-debugging-port=9222'
-]
+# Setup Chrome options
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--window-size=1200x1200")
 
-for option in options:
-    chrome_options.add_argument(option)
+# Initialize WebDriver
+driver = webdriver.Chrome(options=chrome_options)
 
-    
-driver = webdriver.Chrome(options = chrome_options)
-
-driver.get('http://github.com')
-print(driver.title)
-with open('./GitHub_Action_Results.txt', 'w') as f:
-    f.write(f"This was written with a GitHub action {driver.title}")
-
+try:
+    driver.get('http://github.com')
+    print(driver.title)
+    with open('./GitHub_Action_Results.txt', 'w') as f:
+        f.write(f"This was written with a GitHub action. Page title: {driver.title}")
+finally:
+    driver.quit()
+    display.stop()
